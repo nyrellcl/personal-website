e2b1acd8925653fcdbedbaaabd2a2b59aefa0187
nyrellcl <nyrell.leonor2000@gmail.com>
2023-05-06 14:11:49 -0700

    change factor to 1.5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -74,7 +74,7 @@ export default function Home() {
                 }
               />
             </ParallaxLayer>
-            <ParallaxLayer offset={2} speed={1} factor={1.1}>
+            <ParallaxLayer offset={2} speed={1} factor={1.5}>
               <Projects
                 button={
                   <button
