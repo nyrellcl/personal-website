5580be60744e7b98d0a486479880b10a1a7b2abd
nyrellcl <nyrell.leonor2000@gmail.com>
2023-05-10 08:26:43 -0700

    edit mobile and laptop view

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -18,29 +18,23 @@ function Contact({ button }: ButtonProps) {
             My goal is to work alongside talented people and contribute to
             products that enhances the lives of users.
           </p>
-          <br/>
+          <br />
           <p>
             I am always looking to learn more and am open to taking on
             challenging roles that strengthens my skills as a developer.
           </p>
+          <div className="contact-section__content__btn">{button}</div>
           <article className="contact-section__content__mail">
             <article className="contact-section__content__mail__info">
-            <h4>Get in touch</h4>
-            <Link href="mailto:nyrell.leonor2000@gmail.com?subject=%20Connect%20with%20me%20">
+              <h4>Get in touch</h4>
+              <Link href="mailto:nyrell.leonor2000@gmail.com?subject=%20Connect%20with%20me%20">
                 <button type="button">Contact</button>
-           
-            </Link>
-
+              </Link>
             </article>
-            <div className="contact-section__content__mail__btn">
-          {button}
-
-            </div>
           </article>
         </article>
       </section>
       <Footer />
-      
     </>
   );
 }
